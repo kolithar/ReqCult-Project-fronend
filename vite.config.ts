8ae90bf4e11e700b44a8806cc09a12359d6c7914
Kolitha Rathnayake <kolitharathnayake229@gmail.com>
2025-12-30 20:19:45 +0530

    full drshbord update

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-
-    tsconfigPaths(),
+      react()
   ],
-});
-
-
-
+  server: {
+    port: 5173,
+    strictPort: true
+  }
+})
 
 
